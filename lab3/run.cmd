a227d8a419274dbdec094bc5de63c2653ee1ac4c
MaksKuchuk <maks.kuchuk.2016@gmail.com>
2025-01-14 02:56:31 +1000

    lab3

---
+++ lab3/run.cmd
@@ -0,0 +1,4 @@
+cd build
+main.exe ./prog.exe
+main.exe ./error_prog.exe
+cd ..
---
+++ lab3/src/main.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include "lib/background.hpp"
+
+int main(int argc, char* argv[]) {
+    
+    return 0;
+}
