727898709284cca141f8ff1f3c7d3ec6810d4b3e
MaksKuchuk <maks.kuchuk.2016@gmail.com>
2025-01-20 16:05:11 +1000

    1

--- lab6-7/src/main.cpp
+++ lab6-7/src/main.cpp
@@ -6,7 +6,10 @@ int main(int argc, char* argv[])
     QApplication app(argc, argv);
 
     MainWindow test;
-    test.show();
+
+    test.setWindowFlag(Qt::WindowStaysOnTopHint);
+    test.showFullScreen();
+    // test.show();
 
     app.exec();
 }
